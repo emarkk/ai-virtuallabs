c2bfcf3dff459767b5d7137ac018303725df8f3f
emarkk <ema9628@gmail.com>
2020-07-07 16:32:28 +0200

    [Backend] Add professor controller

---
+++ backend/src/main/java/it/polito/ai/virtuallabs/backend/controllers/ProfessorController.java
@@ -0,0 +1,46 @@
+package it.polito.ai.virtuallabs.backend.controllers;
+
+import it.polito.ai.virtuallabs.backend.dtos.CourseDTO;
+import it.polito.ai.virtuallabs.backend.dtos.ProfessorDTO;
+import it.polito.ai.virtuallabs.backend.services.ProfessorNotFoundException;
+import it.polito.ai.virtuallabs.backend.services.ProfessorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/professors")
+public class ProfessorController {
+
+    @Autowired
+    private ProfessorService professorService;
+
+    @GetMapping({ "", "/" })
+    public List<ProfessorDTO> all() {
+        return professorService.getAllProfessors();
+    }
+
+    @GetMapping("/{id}")
+    public ProfessorDTO getOne(@PathVariable("id") Long id) {
+        Optional<ProfessorDTO> professorOptional = professorService.getProfessor(id);
+
+        if(professorOptional.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Professor '" + id + "' not found");
+
+        return professorOptional.get();
+    }
+
+    @GetMapping("/{id}/courses")
+    public List<CourseDTO> getCourses(@PathVariable("id") Long id) {
+        try {
+            return professorService.getCoursesForProfessor(id);
+        } catch(ProfessorNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Professor '" + id + "' not found");
+        }
+    }
+
+}
