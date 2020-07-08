7b0fe56f2e85976cbccb7676d99b02d06a04d88d
emarkk <ema9628@gmail.com>
2020-07-08 11:59:11 +0200

    [Backend] Add DTOs

---
+++ backend/src/main/java/it/polito/ai/virtuallabs/backend/dtos/StudentDTO.java
@@ -0,0 +1,20 @@
+package it.polito.ai.virtuallabs.backend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class StudentDTO {
+
+    private Long id;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String picturePath;
+
+}
