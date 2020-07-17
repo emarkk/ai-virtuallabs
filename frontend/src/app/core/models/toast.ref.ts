129b416bd93ad6a265a2b8dc32111e20cfa983dd
emarkk <ema9628@gmail.com>
2020-07-17 09:02:07 +0200

    [Frontend] Update course detail for students

--- frontend/src/app/core/models/toast.ref.ts
+++ frontend/src/app/core/models/toast.ref.ts
@@ -1,3 +1,6 @@
+// heavily inspired by
+// https://medium.com/angular-in-depth/creating-a-toast-service-with-angular-cdk-a0d35fd8cc12
+
 import { OverlayRef } from '@angular/cdk/overlay';
 
 export class ToastRef {
--- frontend/src/app/student/course-detail/course-detail.component.ts
+++ frontend/src/app/student/course-detail/course-detail.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { Course } from 'src/app/core/models/course.model';
+import { Professor } from 'src/app/core/models/professor.model';
+import { CourseService } from 'src/app/core/services/course.service';
+
+import { navHome, navCourses, nav } from '../student.navdata';
 
 @Component({
   selector: 'app-student-course-detail',
@@ -6,11 +14,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-detail.component.css']
 })
 export class StudentCourseDetailComponent implements OnInit {
+  courseCode: string;
+  courseName: string;
+  courseEnabled: boolean;
+  course$: Observable<Course>;
+  navigationData: Array<any>|null = null;
 
-  constructor() {
+  professors$: Observable<Professor[]>;
+
+  constructor(private route: ActivatedRoute, private courseService: CourseService) {
   }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.courseCode = params.code;
+      this.course$ = this.courseService.get(this.courseCode);
+      this.professors$ = this.courseService.getProfessors(this.courseCode);
+
+      this.course$.subscribe(course => {
+        this.courseName = course.name;
+        this.courseEnabled = course.enabled;
+        this.navigationData = [navHome, navCourses, nav(course.name, `/professor/course/${course.code}`)];
+      });
+    });
   }
 
 }
