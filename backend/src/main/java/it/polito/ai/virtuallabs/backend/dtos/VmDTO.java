0d47016710bdf75cd72e3393c6368c23ba0ca4a8
emarkk <ema9628@gmail.com>
2020-07-29 08:55:44 +0200

    [Backend] Add owners ids list to VmDTO

--- backend/src/main/java/it/polito/ai/virtuallabs/backend/dtos/VmDTO.java
+++ backend/src/main/java/it/polito/ai/virtuallabs/backend/dtos/VmDTO.java
@@ -1,16 +1,31 @@
 package it.polito.ai.virtuallabs.backend.dtos;
 
+import it.polito.ai.virtuallabs.backend.entities.Student;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class VmDTO {
+
     private Long id;
+
     private Integer vCpus;
+
     private Integer diskSpace;
+
     private Integer ram;
+
     private Boolean online;
+
+    private List<Long> owners;
+
+    public void setOwners(List<Student> students) {
+        this.owners = students.stream().map(Student::getId).collect(Collectors.toList());
+    }
 }
