8d4b5e01cdd55cdbe47a868a5b713c0510f668cd
emarkk <ema9628@gmail.com>
2020-07-30 22:29:16 +0200

    [Frontend] Initial layout of VM page

--- frontend/src/app/student/vm/vm.component.ts
+++ frontend/src/app/student/vm/vm.component.ts
@@ -3,8 +3,12 @@ import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { Course } from 'src/app/core/models/course.model';
+import { Vm } from 'src/app/core/models/vm.model';
+import { Team } from 'src/app/core/models/team.model';
 
+import { AuthService } from 'src/app/core/services/auth.service';
 import { CourseService } from 'src/app/core/services/course.service';
+import { StudentService } from 'src/app/core/services/student.service';
 
 import { navHome, navCourses, nav } from '../student.navdata';
 
@@ -17,19 +21,26 @@ export class StudentVmComponent implements OnInit {
   courseCode: string;
   courseName: string;
   course$: Observable<Course>;
+
+  vmId: number;
+  team$: Observable<Team>;
+
   navigationData: Array<any>|null = null;
 
-  constructor(private route: ActivatedRoute, private courseService: CourseService) {
+  constructor(private route: ActivatedRoute, private authService: AuthService, private courseService: CourseService, private studentService: StudentService) {
   }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.courseCode = params.code;
       this.course$ = this.courseService.get(this.courseCode);
+
+      this.vmId = params.id;
+      this.team$ = this.studentService.getJoinedTeamForCourse(this.authService.getId(), this.courseCode);
       
       this.course$.subscribe(course => {
         this.courseName = course.name;
-        this.navigationData = [navHome, navCourses, nav(course.name, `/student/course/${course.code}`), nav('VMs', `/student/course/${course.code}/vms`), nav('VM')];
+        this.navigationData = [navHome, navCourses, nav(course.name, `/student/course/${course.code}`), nav('VMs', `/student/course/${course.code}/vms`)];
       });
     });
   }
