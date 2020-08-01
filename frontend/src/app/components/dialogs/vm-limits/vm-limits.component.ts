96c585c7a3fe02c89024c3c4b56c34e712276837
emarkk <ema9628@gmail.com>
2020-08-01 12:02:52 +0200

    [Frontend] Add VM limit form

--- frontend/src/app/components/dialogs/vm-limits/vm-limits.component.ts
+++ frontend/src/app/components/dialogs/vm-limits/vm-limits.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 export interface VmLimitsDialogData {
   teamName: string
@@ -13,10 +14,33 @@ export interface VmLimitsDialogData {
 export class VmLimitsDialog implements OnInit {
   // whether it is possible to edit the form or not
   locked: boolean = false;
+  // form fields for vm limits editing
+  form = new FormGroup({
+    maxVcpu: new FormControl({ value: '', disabled: false }, [Validators.required]),
+    maxDisk: new FormControl({ value: '', disabled: false }, [Validators.required]),
+    maxRam: new FormControl({ value: '', disabled: false }, [Validators.required]),
+    maxActiveInstances: new FormControl({ value: '', disabled: false }, [Validators.required]),
+    maxInstances: new FormControl({ value: '', disabled: false }, [Validators.required])
+  });
   
   constructor(public dialogRef: MatDialogRef<VmLimitsDialog>, @Inject(MAT_DIALOG_DATA) public data: VmLimitsDialogData) {}
 
   ngOnInit(): void {
   }
 
+  getMaxVcpuErrorMessage() {
+
+  }
+  getMaxDiskErrorMessage() {
+    
+  }
+  getMaxRamErrorMessage() {
+    
+  }
+  getMaxActiveInstancesErrorMessage() {
+
+  }
+  getMaxInstancesErrorMessage() {
+    
+  }
 }
