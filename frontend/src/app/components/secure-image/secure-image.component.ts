a8346e63e39ba87064c6e44f3b1bcf33b356a321
emarkk <ema9628@gmail.com>
2020-09-17 15:16:28 +0200

    [Frontend] Improve profile picture appearance

--- frontend/src/app/components/secure-image/secure-image.component.ts
+++ frontend/src/app/components/secure-image/secure-image.component.ts
@@ -11,13 +11,23 @@ import { ImageService } from 'src/app/core/services/image.service';
   styleUrls: ['./secure-image.component.css']
 })
 export class SecureImageComponent implements OnInit {
+  loaded: boolean = false;
   imageSrc: string = null;
 
   @Input() set user(value: Student|Professor) {
-    if(value instanceof Student && value.hasPicture)
-      this.imageService.getStudentProfilePicture(value.id).subscribe(src => this.imageSrc = src);
-    else if(value instanceof Professor && value.hasPicture)
-      this.imageService.getProfessorProfilePicture(value.id).subscribe(src => this.imageSrc = src);
+    if(value instanceof Student && value.hasPicture) {
+      this.imageService.getStudentProfilePicture(value.id).subscribe(src => {
+        this.loaded = true;
+        this.imageSrc = src;
+      });
+    } else if(value instanceof Professor && value.hasPicture) {
+      this.imageService.getProfessorProfilePicture(value.id).subscribe(src => {
+        this.loaded = true;
+        this.imageSrc = src;
+      });
+    } else if(value) {
+      this.loaded = true;
+    }
   }
 
   constructor(private imageService: ImageService) {
